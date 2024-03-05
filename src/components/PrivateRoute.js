3635cf8d7850031591eddaf6af8521b6933b0a5f
Wassila-Baz <wassila.baz@icloud.com>
2024-03-05 09:48:47 +0100

    projet terminé

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 
 const PrivateRoute = () => {
-  let token = localStorage.getItem("token");
+  let token = localStorage.getItem("token"); // récupère la valeur stockée sous la clé "token" dans le stockage local du navigateur
   
   if (!token) {
-    token = sessionStorage.getItem("token");
+    token = sessionStorage.getItem("token"); //Si aucune valeur n'est trouvée dans le stockage local, le code vérifie le stockage de session pour la clé "token"
   }
 
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  return token ? <Outlet /> : <Navigate to="/sign-in" />;
 };
 
 export default PrivateRoute;
